ccef9511e449c23e2ba2f94844a902869648f623
santiagoc <corregesantiago@gmail.com>
2023-08-05 11:04:55 -0300

    Implementada solucion para evitar que se dispare el evento click del div

--- eventos.js
+++ eventos.js
@@ -1,4 +1,9 @@
 
-const div = document.addEventListener('click', () => {
+document.getElementById("cont-principal").addEventListener('click', () => {
     alert("Hola soy el div")
+})
+
+document.getElementById("btn-saludar").addEventListener('click', () => {
+    event.stopPropagation(); // Detener la propagación del evento click del botón
+    alert('Hola!');
 })
